a42a42c3ae6019d27724c5238ecb7834e47997cb
Adrian Jez <jadi26@wp.pl>
2021-09-06 22:52:50 +0200

    Add new files

---
+++ Calculator.ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Calculator.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj w aplikacji kalkulator");
+            Console.WriteLine("Wpisz 1. liczbe: ");
+            var number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Jaka operacje chcesz wykonac");
+            Console.WriteLine(" '+','-', '*', '/'");
+            var operation = Console.ReadLine();
+            Console.WriteLine("Podaj 2. liczbe");
+            var number2 = int.Parse(Console.ReadLine());
+
+            var result = 0;
+
+            switch(operation)
+            {
+                case "+":
+                    result = number1 + number2;
+                break;
+                case "-":
+                    result = number1 - number2;
+                    break;
+                case "*":
+                    result = number1 * number2;
+                    break;
+                case "/":
+                    result = number1 / number2;
+                    break;
+                default:
+                    throw new Exception("zla opcja");
+            }
+            Console.WriteLine($"Wynik Twojego dzialania to: {result}");
+        }
+    }
+}
